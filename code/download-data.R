c66010d2f503988423bd6fc1612a2eb71df78b80
mpadge <mark.padgham@email.com>
2017-11-14 17:30:18 +0000

    set data chunk sizes

--- code/download-data.R
+++ code/download-data.R
@@ -1,12 +1,12 @@
 devtools::load_all ()
 
 message ("getting kathmandu streets ... ", appendLF = FALSE)
-get_who_streets (city = "kathmandu")
+get_who_streets (city = "kathmandu", n = 1)
 message ("\ndone; getting kathmandu buildings ... ", appendLF = FALSE)
-get_who_buildings (city = "kathmandu")
+get_who_buildings (city = "kathmandu", n = 2)
 
 message ("\ndone; getting accra streets ... ", appendLF = FALSE)
-get_who_streets (city = "accra")
+get_who_streets (city = "accra", n = 1)
 message ("\ndone; getting accra buildings ... ", appendLF = FALSE)
-get_who_buildings (city = "accra")
+get_who_buildings (city = "accra", n = 1)
 message ("\ndone")
