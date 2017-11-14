79905e39e9ca17cd43176487f5e04b2d28d0af83
mpadge <mark.padgham@email.com>
2017-11-14 13:36:46 +0000

    improve get_who_data fn

--- R/download-data.R
+++ R/download-data.R
@@ -5,17 +5,23 @@
 #' @param city Name of city for which data are to be obtained
 #' @param key List of OSM keys
 #' @param value List of corresponding OSM values
+#' @return The \pkg{sf}-formatted data object (invisibly)
 #'
 #' @note \code{value} must either be \code{NULL}, or have the same length as
 #' \code{key}. To return all values for one particular \code{key} from a vector,
 #' set corresponding \code{value = ""}.
 #'
 #' @export
-get_who_data <- function (city = "kathmandu", key, value)
+get_who_data <- function (city = "kathmandu", key = NULL, value = NULL)
 {
     region_shape <- getbb(place_name = city, format_out = "polygon")
     if (is.list (region_shape))
         region_shape <- region_shape [[1]]
+
+    if (length (key) == 1 & is.null (value))
+        value <- ""
+    else if (length (key) != length (value))
+        stop ("value must have same length as key")
 
     q <- opq (bbox = city)
     for (i in seq (key))
@@ -26,9 +32,14 @@ get_who_data <- function (city = "kathmandu", key, value)
     nm <- paste0 (city, "_", key [1])
     assign (nm, dat)
     data_dir <- get_who_data_dir (city = city)
-    fname <- file.path (data_dir, paste0 (city, "-", value, "-", key, ".Rds"))
+    fname <- file.path (data_dir, paste0 (city, "-", value))
+    if (value [1] != "")
+        fname <- paste0 (fname, "-", key)
+    fname <- paste0 (fname, ".Rds")
     saveRDS (get (nm), fname)
     message ("saved ", fname)
+
+    invisible (dat)
 }
 
 #' get_who_data_dir
