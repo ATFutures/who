48e58d2bf7cf3549b97fa1485cc7f446bb9d43db
mpadge <mark.padgham@email.com>
2017-12-07 12:14:43 +0100

    rm R/download-data (it's in popdens now)

--- R/who-package.R
+++ R/who-package.R
@@ -3,7 +3,6 @@
 #' @name who
 #' @docType package
 #' @author Mark Padgham, Robin Lovelace
-#' @importFrom here here
-#' @importFrom magrittr %>% extract2
 #' @importFrom osmdata add_osm_feature opq osmdata_sf trim_osmdata
+#' @importFrom utils packageVersion
 NULL
