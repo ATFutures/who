e4404fae6f560f83e53f84770ca64b185b312684
robinlovelace <rob00x@gmail.com>
2017-12-07 18:27:33 +0000

    Update node allocation code

--- code/assign-pops-to-nodes.R
+++ code/assign-pops-to-nodes.R
@@ -1,13 +1,22 @@
 library(tidyverse)
+devtools::install_github("robinlovelace/osmdata")
 library(osmdata)
 library(raster)
+library(tmap)
 library(sf)
 
-boundary = getbb("Kathmandu", format_out = )
+boundary_bb = getbb("Accra")
+boundary = stplanr::bb2poly(boundary_bb) %>% 
+  st_as_sf()
 pd = raster::raster("../who-data/accra/popdens/GHA15_040213.tif")
-summary(pd)
+pd # 200k cells
+# pd = mask(pd, as_Spatial(st_geometry(boundary)))
+pd = crop(pd, extent(boundary_bb)) # make smaller dataset - 148k cells
+
 pd_sf = pd %>% 
   raster::rasterToPolygons() %>% 
   st_as_sf()
-mapview::mapview(pd) # nice viz
+tmap_mode("view")
+qtm(pd) +
+  qtm(boundary)
 nodes = readRDS("../who-data/accra/osm/accra-hw.Rds")
