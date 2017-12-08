167946f3563dd31354cc19416608f1896e1b5eaa
Robin Lovelace <rob00x@gmail.com>
2017-12-08 09:56:47 +0000

    Add piper operator

--- R/who-package.R
+++ R/who-package.R
@@ -6,3 +6,13 @@
 #' @importFrom osmdata add_osm_feature opq osmdata_sf trim_osmdata
 #' @importFrom utils packageVersion
 NULL
+#' Pipe operator
+#'
+#' @name %>%
+#' @rdname pipe
+#' @keywords internal
+#' @export
+#' @importFrom dplyr %>%
+#' @usage lhs \%>\% rhs
+NULL
+utils::globalVariables(c(".", "n"))
