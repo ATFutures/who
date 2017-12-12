0b6656886fafc013c0c0dcfa9ff48d51c3ce7b6a
Robin Lovelace <rob00x@gmail.com>
2017-12-12 18:43:54 +0000

    Add SI test

---
+++ tests/si-test.R
@@ -0,0 +1,25 @@
+devtools::install_github("ATFutures/dodgr")
+library(dodgr)
+library(sf)
+
+# load data
+net <- readRDS ("../who-data/accra/osm/accra-hw.Rds") %>% weight_streetnet ()
+plot(net)
+nodes <- readRDS ("../who-data/accra/osm/nodes_new.Rds")
+verts <- dodgr_vertices (net)
+
+indx <- 1:100
+nodes <- nodes [indx, ]
+indx <- match_pts_to_graph (verts, nodes)
+dcol <- names (nodes) [which (grepl ("GHA15", names (nodes)))]
+dens <- nodes [[dcol]]
+
+# remove duplicated points
+dens <- dens [which (!duplicated (indx))]
+id <- verts$id [unique (indx)]
+indx <- which (!is.na (dens))
+id <- id [indx]
+dens <- dens [indx]
+s <- dodgr_spatial_interaction (net, nodes = id, dens = dens, k = 2)
+f = dodgr_flows(graph = net, from = id, to = id, flows = s)
+dodgr_flowmap(f, "/tmp/flow.png")
