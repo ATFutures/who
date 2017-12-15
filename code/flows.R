861475424c48e26c571cd506af75bc8a8e304b80
mpadge <mark.padgham@email.com>
2017-12-15 12:23:42 +0100

    convert final code/flows.R result to sf object

--- code/flows.R
+++ code/flows.R
@@ -21,21 +21,34 @@ xy <- data.frame (x = x, y = y)
 transport <- "bicycle" # for weighting profile
 net <- readRDS (file.path (data_dir, "accra", "osm", "accra-hw.Rds")) %>%
     weight_streetnet (wt_profile = transport)
-# match xy to network:
-verts <- dodgr_vertices (net)
+# match xy to network. Although dodgr can make contracted graphs that include
+# explicitly specified points, the `dodgr_to_sf` function works only with the
+# fully contracted graph. This means that the density vertices need to be
+# matched to the contracted graph, not the full graph.
+net_c <- dodgr_contract_graph (graph = net)$graph
+verts <- dodgr_vertices (net_c)
 xy_index <- match_pts_to_graph (verts = verts, xy = xy)
-#xy_id <- verts$id [xy_index]
 
-od <- dodgr_spatial_interaction (graph = net, nodes = xy_index, dens = dens,
+od <- dodgr_spatial_interaction (graph = net_c, nodes = xy_index, dens = dens,
                                  k = 2, contract = TRUE)
 
-# Then get and plot flows
-flows <- dodgr_flows (graph = net, from = xy_index, to = xy_index, flows = od) %>%
-    merge_directed_flows ()
-dodgr_flowmap (flows, file = file.path (here::here(), "fig", "test"))
+# Then get and plot flows. Although not necessary here, this also demonstrates
+# how to convert the `xy_index` of routing vertices to explicit OSM ID values
+# which can also be (more safely) submitted to all dodgr routines.
+xy_id <- verts$id [xy_index]
+flows <- dodgr_flows (graph = net_c, from = xy_id, to = xy_id, flows = od)
+# merge directed flows for plotting only:
+flows_merged <- merge_directed_flows (flows)
+dodgr_flowmap (flows_merged, file = file.path (here::here(), "fig", "test"))
 
 # or zoom in to verts only (makes no diff here):
 #bb <- apply (verts [xy_index, 2:3], 2, range)
 #rownames (bb) <- c ("x", "y")
 #colnames (bb) <- c ("min", "max")
 #dodgr_flowmap (flows, file = file.path (here::here(), "fig", "test"), bbox = bb)
+
+# finally, the sf object corresponding to the flows. Note this requires the full
+# network, and will generate an sfc object with exactly the same number of items
+# as rows in `net_c`.
+sf_xy <- dodgr_to_sf (net)
+identical (length (sf_xy), nrow (net_c)) # TRUE!
